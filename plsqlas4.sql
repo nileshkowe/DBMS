eff8040e5dc19a82ea94feb4cb809bf24145e9a7
nileshkowe <91322589+nileshkowe@users.noreply.github.com>
2022-09-14 10:56:18 +0530

    Update and rename plsqlas4 to plsqlas4.sql

--- plsqlas4.sql
+++ plsqlas4.sql
@@ -3,6 +3,8 @@ create table A_circle (rad integer, area float);
 select * from A_circle;
 */
 
+
+-- Using While loop
 DECLARE
   area number(5,2);
   rad number:=5;
@@ -13,5 +15,18 @@ BEGIN
    area:=3.14*rad*rad;
    insert into A_circle values (rad, area);
    rad:=rad+1;
+  END LOOP;
+END;
+
+
+-- Using For loop
+DECLARE
+  area number(5,2);
+  rad number:=5;
+
+BEGIN 
+  FOR rad IN 5..9 LOOP
+   area:=3.14*rad*rad;
+   insert into A_circle values (rad, area);
   END LOOP;
 END;
